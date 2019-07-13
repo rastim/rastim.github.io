f211a018b1157443ae192b30a572a3731708f1ac
rastim <rastim88@gmail.com>
2019-07-13 12:17:32 +0700

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4>About</h4>
+            <p>Home Page ~ Materialize comes in two different forms. You can select which version you want depending on your preference and expertise. To start using Materialize, all you have to do is download one of the options below.</p>
+        </div>
+    )
+}
+
+export default Rainbow(About)
